14008d797aa43fe11daaad3f80f99ab5325b3c28
David Rio Deiros <driodeiros@gmail.com>
2023-01-27 15:23:36 -0500

    rm ec2

--- main.tf
+++ main.tf
@@ -25,14 +25,14 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-  owners = ["099720109477"]
-}
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+#   }
+#   owners = ["099720109477"]
+# }
 
 # resource "aws_instance" "instance" {
 #   ami           = data.aws_ami.ubuntu.id
