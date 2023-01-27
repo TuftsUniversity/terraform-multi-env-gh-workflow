de2465b7ebad5e98749edb0d0207bfabc35c271a
David Rio Deiros <driodeiros@gmail.com>
2023-01-27 09:37:05 -0500

    remove instance

--- main.tf
+++ main.tf
@@ -41,11 +41,3 @@ resource "aws_instance" "instance" {
     Name = terraform.workspace
   }
 }
-
-resource "aws_instance" "instance_2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  tags = {
-    Name = "foobar"
-  }
-}
