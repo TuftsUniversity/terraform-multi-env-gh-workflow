f6e47b7cf59c377f84a3d427443f4a749610e05e
David Rio Deiros <driodeiros@gmail.com>
2023-01-27 09:32:06 -0500

    fix the name

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "aws_instance" "instance" {
+resource "aws_instance_2" "instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   tags = {
