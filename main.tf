1a7d878cba2ab807d0f12197c39438ccc12da0d9
David Rio Deiros <driodeiros@gmail.com>
2023-01-27 15:14:10 -0500

    remove vms

--- main.tf
+++ main.tf
@@ -25,19 +25,19 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-  owners = ["099720109477"]
-}
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+#   }
+#   owners = ["099720109477"]
+# }
 
-resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  tags = {
-    Name = "hello-new-instance"
-  }
-}
+# resource "aws_instance" "instance" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "hello-new-instance"
+#   }
+# }
